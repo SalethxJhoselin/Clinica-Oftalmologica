d5ec2063eba6cfa3fb744e4c0ff4e455e0de6585
Saleth Jhoselin <mamjhoss@gmail.com>
2024-09-10 12:49:47 -0600

    Add login functionality to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Layout/Navbar';
+//import Navbar from './components/Layout/Navbar';
 import MyRoutes from './routes/routes';
 import Sidebar from './components/Layout/Sidebar';
 import { AuthProvider, useAuth } from './components/users/AuthContext';
+
 
 const AppContent = () => {
   const { isLoggedIn, sidebarOpen, setSidebarOpen } = useAuth();
@@ -14,7 +14,7 @@ const AppContent = () => {
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       )}
       <div className="flex-1 flex flex-col overflow-x-hidden">
-        <Navbar />
+        {/*<Navbar />*/}
         <MyRoutes />
       </div>
     </div>
