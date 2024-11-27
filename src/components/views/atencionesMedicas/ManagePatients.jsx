0f4a611e359b542e063471aed205608f948794ee
Saleth Jhoselin <mamjhoss@gmail.com>
2024-11-27 10:32:57 -0600

    historial por id, conectar al backend

--- src/components/views/atencionesMedicas/ManagePatients.jsx
+++ src/components/views/atencionesMedicas/ManagePatients.jsx
@@ -38,18 +38,22 @@ const ManagePatients = () => {
     };
 
     const handleDeleteConfirm = async () => {
-        try { 
+        try {
             await deletePatient(patientToDelete.id); // API de eliminación de paciente
             message.success('Paciente eliminado exitosamente');
             setPatients(prevPatients => prevPatients.filter(patient => patient.id !== patientToDelete.id));
             setDeleteModalVisible(false);
-        } catch(error) {
+        } catch (error) {
             message.error('Error al eliminar paciente');
         }
     };
 
     const handleDeleteCancel = () => {
         setDeleteModalVisible(false);
+    };
+
+    const handleViewHistory = (id) => {
+        console.log("ID del paciente seleccionado:", id);
     };
 
     const columns = [
@@ -63,11 +67,6 @@ const ManagePatients = () => {
             key: 'nombre_completo',
             render: (text, record) => `${record.nombre} ${record.apellido_paterno} ${record.apellido_materno}`,
         },
-        // {
-        //     title: 'Tipo de Sangre',
-        //     dataIndex: 'tipo_sangre',
-        //     key: 'tipo_sangre',
-        // },
         {
             title: 'Estado',
             dataIndex: 'estado',
@@ -80,51 +79,41 @@ const ManagePatients = () => {
             render: (_, record) => (
                 <Space>
                     <Button type="primary" onClick={() => handleShowDetail(record)}><EditOutlined /></Button>
-                    {/* <Button
-                        style={{
-                            backgroundColor: '#F44336',
-                            color: '#fff',
-                            borderRadius: '10px',
-                            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-                        }}
-                        onClick={() => { setPatientToDelete(record); setDeleteModalVisible(true); }}
-                    >
-                        <DeleteOutlined />
-                    </Button> */}
+                    <Button onClick={() => handleViewHistory(record.paciente_id)}>Ver Historial</Button>
                 </Space>
             ),
         }
     ];
 
     return (
-    <div className="p-5 bg-white rounded-2xl shadow-lg mt-2 ml-2 mr-2">
-        <Title level={3} className="text-center">Administrar Pacientes</Title>
-        <div className="flex justify-end mb-6">
-            <RegisterPatient /> {/* Cambiamos a registro de pacientes */}
-        </div>
-        <Table
-            columns={columns}
-            dataSource={patients}
-            rowKey="id"
-            pagination={{ pageSize: 4, size: 'small' }}
-        />
-        <Modal
-            title="Eliminar Paciente"
-            visible={deleteModalVisible}
-            onOk={handleDeleteConfirm}
-            onCancel={handleDeleteCancel}
-        >
-            <p>¿Está seguro que desea eliminar a {patientToDelete?.nombre} {patientToDelete?.apellido_paterno}?</p>
-        </Modal>
-        {isDetailModalVisible && selectedPatient && (
-            <PatientDetail
-                visible={isDetailModalVisible}
-                onClose={handleDetailModalClose}
-                patient={selectedPatient} // Cambiamos 'user' por 'patient'
+        <div className="p-5 bg-white rounded-2xl shadow-lg mt-2 ml-2 mr-2">
+            <Title level={3} className="text-center">Administrar Pacientes</Title>
+            <div className="flex justify-end mb-6">
+                <RegisterPatient /> {/* Cambiamos a registro de pacientes */}
+            </div>
+            <Table
+                columns={columns}
+                dataSource={patients}
+                rowKey="id"
+                pagination={{ pageSize: 4, size: 'small' }}
             />
-        )}
-    </div>
-);
+            <Modal
+                title="Eliminar Paciente"
+                visible={deleteModalVisible}
+                onOk={handleDeleteConfirm}
+                onCancel={handleDeleteCancel}
+            >
+                <p>¿Está seguro que desea eliminar a {patientToDelete?.nombre} {patientToDelete?.apellido_paterno}?</p>
+            </Modal>
+            {isDetailModalVisible && selectedPatient && (
+                <PatientDetail
+                    visible={isDetailModalVisible}
+                    onClose={handleDetailModalClose}
+                    patient={selectedPatient} // Cambiamos 'user' por 'patient'
+                />
+            )}
+        </div>
+    );
 };
 
 export default ManagePatients;
